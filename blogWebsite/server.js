ede8c6eaa4bda2f6c445c732d1b99e235f7e2e62
Tanvi Gupta <tanvi05@gmail.com>
2025-03-02 00:06:57 +0530

    added route to server.js

--- blogWebsite/server.js
+++ blogWebsite/server.js
@@ -7,6 +7,8 @@ app.use("/api/users",require("./routes/user"))
 app.use("/api/blogs",require("./routes/blog"))
 app.use("/api/auth",require("./routes/auth"))
 app.use("/api/like",require("./routes/like"));
+app.use("/api/dislike",require("./routes/dislike"))
+app.use("/verify",require("./routes/verify"))
 
 app.listen(4545,()=>{
     console.log("server started");
